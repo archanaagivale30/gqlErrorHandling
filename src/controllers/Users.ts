a67ff08dbca60a463237e07ece4d3db36c2585e6
Archana Agivale <archana.agivale@qed42.com>
2020-11-27 12:01:58 +0530

    unwanted code removed

--- src/controllers/Users.ts
+++ src/controllers/Users.ts
@@ -1,48 +1,6 @@
 import UserModel, { IUserModel } from '../models/UserModel';
-import * as express from 'express';
 const {errorName} =require("../config/errorContant");
 class UserController {
-
-  
-    /**
-     * @api {get} /user Get all users
-     * @apiName GetUser
-     * @apiGroup User
-     *
-     * @apiParam {Number} id Users unique ID.
-     *
-     * @apiSuccess {String} firstname Firstname of the User.
-     * @apiSuccess {String} lastname  Lastname of the User.
-     *
-     * @apiSuccessExample Success-Response:
-     *     HTTP/1.1 200 OK
-     *     {
-     *       "firstname": "John",
-     *       "lastname": "Doe"
-     *     }
-     *
-     * @apiError UserNotFound The id of the User was not found.
-     *
-     * @apiErrorExample Error-Response:
-     *     HTTP/1.1 404 Not Found
-     *     {
-     *       "error": "UserNotFound"
-     *     }
-     */
-    public getAllUsers(req, res, next): void {
-			UserModel
-				.find({})
-				.then((data)=> {
-					res.status(200).json({data});
-				})
-				.catch((error: Error) => {
-					res.status(500).json({
-						error: error.message,
-						errorStack: error.stack
-					});
-					next(error);
-				});
-    }
     public getListUsers({filter}, req): Array<IUserModel> | Object {
        
         return UserModel
@@ -56,9 +14,7 @@ class UserController {
             throw err;
           });
       }
-      
-     
-    
+        
     public getUser({id}, req): any {
 			
       return UserModel
@@ -72,6 +28,7 @@ class UserController {
             throw err;
       });
     }
+    
     public updateUser({id,input}): any {
         let updatePayload: any = {};
         return UserModel.update({_id:id},input)
